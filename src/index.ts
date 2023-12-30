6e2eef0ce0381d33669562bf2b0d9dec58b774d4
DEPSTRCZ <jiri.edelmann@gmail.com>
2023-12-30 16:42:39 +0100

    Done.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import OvhEngine, { OvhError } from '@ovh-api/api';
+import OvhEngine from '@ovh-api/api';
 import apiIp from '@ovh-api/ip';
-import { ovh, general, embeds } from './conf.json';
+import { ovh, general, embeds, externalPost } from './conf.json';
+import Express, {Request, Response} from 'express';
 
 const ovhEngine = new OvhEngine({
     appKey: ovh.appKey,
@@ -15,59 +16,84 @@ let State = false;
 
 
 async function SendAlert(Type: boolean) {
-    if (Type) {
-        State = true;
+    let ChosenEmbed = Type ? 0 : 1; // Get the embed index from the type 0 = Start, 1 = End
+    State = Type; // Set the state to the type
+    embeds[ChosenEmbed].fields[0].value = `<t:${Math.floor(new Date().getTime() / 1000)}:R>`; // Set the date in the embed to now
 
-        console.log("Attack started", new Date().toLocaleString());
-        embeds[0].fields[0].value = `<t:${Math.floor(new Date().getTime() / 1000)}:R>`;
+    console.log("Sending alert",(Type) ? "Attack has started":"Attack has Ended", new Date().toLocaleString());
+    
+    await fetch(general.WebhookURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            content: null, embeds: [embeds[ChosenEmbed]]
+        })
+    });
+}
 
-        fetch(general.WebhookURL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                content: null, embeds: [embeds[0]]
-            })
-        });
-    } else {
-        State = false;
-
-        console.log("Attack has stopped", new Date().toLocaleString());
-        embeds[1].fields[0].value = `<t:${Math.floor(new Date().getTime() / 1000)}:R>`;
-
-        fetch(general.WebhookURL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                content: null, embeds: [embeds[1]]
-            })
-        });
-    }
+async function ExternalAlert() {
+    //Implement how ever you want.
+    embeds[2].fields[0].value = `<t:${Math.floor(new Date().getTime() / 1000)}:R>`
+    await fetch(general.WebhookURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            content: null, embeds: [embeds[2]]
+        })
+    });
 }
 
 async function QueryOvhAPI() {
     try {
+        console.log("Querying OVH API", new Date().toLocaleString());
         const data = await api.ip.$("51.77.68.20/32").mitigation.$("51.77.68.10").$get();
+        
         // Only work while on Auto mitigation.. The API is not well documented and i am unsure how do the responses work when the mode is pernament. Feel free to open Issue!
         switch (data.state) {
             case "creationPending": if (!State) SendAlert(true); break;
             case "ok": if (!State) SendAlert(true); break;
-            case "removalPending": SendAlert(false); break;
+            case "removalPending": if (State) SendAlert(false); break;
         }
     } catch (error) {
         if (State) {
             SendAlert(false);
-            State = false;
         };
 
 
     }
 
 }
+
+console.log("Started OVH API Monitor");
 setInterval(async () => {
     await QueryOvhAPI();
-
+    
 }, general.interval * 1000);
+
+if (externalPost.enabled) {
+    const app = Express();
+    app.listen(externalPost.port, () => console.log(`Started ExternalPost Listener @ https://<IP>:${externalPost.port}/external`));
+
+    app.post('/external', async (req: Request, res: Response) => {
+        try {
+            const authToken: string | undefined = req.headers.authorization?.split(' ')[1];
+        
+            if (!authToken) res.status(401).send('Unauthorized');
+    
+            if (authToken === externalPost.token) {
+                res.status(200).send('OK');
+                await ExternalAlert();
+
+            } else {
+                res.status(401).send('Unauthorized');
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(500).send('Internal Server Error');
+        };
+    });
+}
